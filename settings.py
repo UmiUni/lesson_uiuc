aef07998a0c9199c89d489cfb62c2c5d1ba26af9
Chaoran Wang <superchaoran@gmail.com>
2018-01-19 04:15:24 +0000

    lesson

---
+++ settings.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+import datetime
+def init():
+
+  global chatGroups
+  global vT
+  global usersDict
+  global admins 
+  global ADMIN
+  global previousDay
+  global mutes
+
+  chatGroups =[
+  u'洛杉矶租房二手万能总群',
+  u'芝加哥租房二手万能总群',
+  u'北美打折总群😊'
+  ]
+
+  v0= u"您好,北美加群小助手Jogchat.com创始人为您服务😊机器人托管中马上就回来哦😯\n"
+  v1= u"回复 0 加洛杉矶租房二手万能总群\n"
+  v2= u"回复 1 加芝加哥租房二手万能总群\n"
+  v3= u"回复 2 加北美打折总群😊\n\n"
+  v4= u"回复 3 关闭自动回复🐱\n"
+  v5= u"回复 4 打开自动回复🐱\n\n"
+  v6= u"UIUC 的校友们可以回复课程编号加课程群(如CS225, ECE120, ECON302, Math241, Stat431)\n\n"
+  v12=u"回复 99 查看【北美加群小助手Jogchat.com】\n 公众号二维码加硅谷、西雅图、三番、UIUC、Purdue等地群\n"
+  vT =v0+v1+v2+v3+v4+v5+v6+v12
+  
+  usersDict = {}
+  admins = []
+  mutes = [] 
+  ADMIN = u'超然'
+  previousDay = datetime.datetime.now().day
---
+++ tiantian.py
@@ -0,0 +1,79 @@
+# -*- coding: UTF-8 -*-
+import requests
+import itchat
+from itchat.content import *
+import sys  
+import json
+import time
+from time import sleep
+import settings
+from xiaozhushou_util.xiaozhushou_util import *
+import re
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+itchat.auto_login(enableCmdQR=2,hotReload=True)
+itchat.get_chatrooms(update=True)
+settings.init()
+
+@itchat.msg_register('Friends')
+def add_friend(msg):
+    itchat.add_friend(**msg['Text'])
+    itchat.send_msg(settings.vT, msg['RecommendInfo']['UserName'])
+
+@itchat.msg_register(itchat.content.TEXT)
+def tuling_reply(msg):
+  CurUserName = msg['FromUserName']
+  sendGroupInviteMsg(msg,CurUserName)
+
+#send group invite msg according to digits
+def sendGroupInviteMsg(msg,CurUserName):
+  msgText = msg['Text']
+  msgText = msgText.strip()
+  msgText = msgText.replace(" ","")
+  x = re.findall(r'\d+', msgText)
+  auto = True 
+  #print x
+  if(len(x) >0):
+    y= int(x[0])
+    if(y>=0 and y<=2):
+      pullMembersMore(msg, settings.chatGroups[y], CurUserName)
+      sleep(0.5)
+    elif(y==3):
+      CurUserName = msg['FromUserName']
+      settings.mutes.append(CurUserName)
+    elif(y==4):
+      CurUserName = msg['FromUserName']
+      if CurUserName in settings.mutes: settings.mutes.remove(CurUserName)
+    elif(y==99):
+      advertiseQR(CurUserName)
+
+  xList = re.findall(r'(?:Cs|cs|CS)\d+', msgText)
+  addToCourse(xList,msg,CurUserName)
+  xList = re.findall(r'(?:Ece|ece|ECE)\d+', msgText)
+  addToCourse(xList,msg,CurUserName)
+  xList = re.findall(r'(?:Stat|STat|STAT|stat)\d+', msgText)
+  addToCourse(xList,msg,CurUserName)
+
+  if CurUserName not in settings.mutes:
+    itchat.send_msg(settings.vT, CurUserName)
+  sleep(0.5)
+
+def addToCourse(iL,msg,CurUserName):
+  if(len(iL) > 0):
+    print iL
+    twoL=re.split('(\d+)',iL[0])
+    print twoL
+    finalC0=twoL[0].upper()+twoL[1]
+    finalC1=twoL[0].upper()+" "+twoL[1]
+
+    retVal0 = pullMembersMore(msg, finalC0, CurUserName)
+    if not retVal0:
+      retVal1 = pullMembersMore(msg, finalC1, CurUserName)
+      if not retVal1:
+        itchat.send_msg("不好意思，此课程群没有，几分钟内帮你建好😊", CurUserName)
+
+    sleep(0.5)
+
+itchat.run() 
+
