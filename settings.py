83247db798c76579228e82624b1c34b260531c86
Chaoran Wang <superchaoran@gmail.com>
2018-01-21 09:13:07 +0000

    add lesson

--- settings.py
+++ settings.py
@@ -23,8 +23,8 @@ def init():
   v4= u"回复 3 关闭自动回复🐱\n"
   v5= u"回复 4 打开自动回复🐱\n\n"
   v6= u"UIUC 的校友们可以回复课程编号加课程群(如CS225, ECE120, ECON302, Math241, Stat431)\n\n"
-  v12=u"回复 99 查看【北美加群小助手Jogchat.com】\n 公众号二维码加硅谷、西雅图、三番、UIUC、Purdue等地群\n"
-  v13=u"回复 100 查看 UIUC经济、统计、CS、物理、心理系大家庭小助手。\n"
+  v12=u"回复 99 查看【北美加群小助手Jogchat.com】\n 公众号二维码加硅谷、西雅图、三番、纽约、Purdue等地群\n\n"
+  v13=u"回复 100 查看 UIUC经济、统计、CS、物理、心理系大家庭小助手。\n\n"
   vT =v0+v1+v2+v3+v4+v5+v6+v12+v13
   
   usersDict = {}
